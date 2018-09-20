dff47668d23516926453b8f94d8b5d4af58ca70f
bbond2980 <bbond2980@gmail.com>
2018-09-19 22:11:26 -0400

    fix loading error

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,6 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 
-@app.route('/login')
-
-
 def index():
 	user = {'username': 'Ben'}
 	posts = [
@@ -26,6 +23,7 @@ def index():
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login')
 def login():
 	form = LoginForm()
 	return render_template('login.html', title='Sign In', form=form)
